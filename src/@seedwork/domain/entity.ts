3d0a51fb24bb1e28ff879231720a8957c4ccede6
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-01-31 12:14:58 -0300

    feat: base entity

---
+++ src/@seedwork/domain/entity.ts
@@ -0,0 +1,17 @@
+import { UniqueEntityId } from "./unique-entity-id.vo";
+
+export abstract class Entity<Props> {
+  public readonly uniqueEntityId: UniqueEntityId;
+
+  constructor(public readonly props: Props, id?: UniqueEntityId) {
+    this.uniqueEntityId = id || new UniqueEntityId();
+  }
+
+  get id() {
+    return this.uniqueEntityId.value;
+  }
+
+  toJSON() {
+    return { id: this.id, ...this.props };
+  }
+}
